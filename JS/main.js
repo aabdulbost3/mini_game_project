88ca8fa055911ddbcc300bec73565b0608ca12e6
Abbdulbosit <96017917+aabdulbost3@users.noreply.github.com>
2022-08-16 20:12:41 +0500

    update js

--- JS/main.js
+++ JS/main.js
@@ -35,6 +35,9 @@ function randomColor() {
     );
 }
 
+
+
+
 // function
 
 function Ball() {
@@ -54,3 +57,53 @@ function Ball() {
     //c.stroke();
   };
 }
+var bal = [];
+for (var i=0; i<50; i++){
+    bal.push(new Ball());
+}
+
+function animate() {    
+  if (tx != window.innerWidth || ty != window.innerHeight) {
+    tx = window.innerWidth;
+    ty = window.innerHeight;
+    canvas.width = tx;
+    canvas.height = ty;
+  }
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, tx, ty);
+  for (var i = 0; i < bal.length; i++) {
+    bal[i].update();
+    bal[i].y += bal[i].dy;
+    bal[i].x += bal[i].dx;
+    if (bal[i].y + bal[i].radius >= ty) {
+      bal[i].dy = -bal[i].dy * grav;
+    } else {
+      bal[i].dy += bal[i].vel;
+    }    
+    if(bal[i].x + bal[i].radius > tx || bal[i].x - bal[i].radius < 0){
+        bal[i].dx = -bal[i].dx;
+    }
+    if(mousex > bal[i].x - 20 && 
+      mousex < bal[i].x + 20 &&
+      mousey > bal[i].y -50 &&
+      mousey < bal[i].y +50 &&
+      bal[i].radius < 70){
+        //bal[i].x += +1;
+        bal[i].radius +=5; 
+      } else {
+        if(bal[i].radius > bal[i].startradius){
+          bal[i].radius += -5;
+        }
+      }
+      
+    //forloop end
+    }
+//animation end
+}
+
+animate();
+
+setInterval(function() {
+  bal.push(new Ball());
+  bal.splice(0, 1);
+}, 400);
