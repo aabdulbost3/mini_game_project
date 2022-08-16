90166837244dd1d8bd787c37e39978c7e5f92992
Abbdulbosit <96017917+aabdulbost3@users.noreply.github.com>
2022-08-16 20:05:39 +0500

    update main js

--- JS/main.js
+++ JS/main.js
@@ -34,3 +34,23 @@ function randomColor() {
         ")"
     );
 }
+
+// function
+
+function Ball() {
+  this.color = randomColor();
+  this.radius = Math.random() * 20 + 14;
+  this.startradius = this.radius;
+  this.x = Math.random() * (tx - this.radius * 2) + this.radius;
+  this.y = Math.random() * (ty - this.radius);
+  this.dy = Math.random() * 2;
+  this.dx = Math.round((Math.random() - 0.5) * 10);
+  this.vel = Math.random() /5;
+  this.update = function() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    c.fillStyle = this.color;
+    c.fill();
+    //c.stroke();
+  };
+}
