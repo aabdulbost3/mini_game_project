a32a04cde6f22f32b19cafee8c08f1f7d2c3bc8d
Abbdulbosit <96017917+aabdulbost3@users.noreply.github.com>
2022-08-16 20:01:17 +0500

    update main js

--- JS/main.js
+++ JS/main.js
@@ -12,7 +12,25 @@ canvas.height = ty;
 var mousex = 0;
 var mousey = 0;
 
-addEventListener("mousemove", function() {
-  mousex = event.clientX;
-  mousey = event.clientY;
+addEventListener("mousemove", function () {
+    mousex = event.clientX;
+    mousey = event.clientY;
 });
+
+
+
+var grav = 0.99;
+c.strokeWidth = 5;
+function randomColor() {
+    return (
+        "rgba(" +
+        Math.round(Math.random() * 250) +
+        "," +
+        Math.round(Math.random() * 250) +
+        "," +
+        Math.round(Math.random() * 250) +
+        "," +
+        Math.ceil(Math.random() * 10) / 10 +
+        ")"
+    );
+}
